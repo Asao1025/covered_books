d1e0ea77b58708926074f0e9a16e848bd25e449e
Asao1025 <asx2858@gmail.com>
2025-01-27 23:58:28 +0900

    setOutputDir

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: `export`
-   distDir: '.vercel/output/static',
+   distDir: '.vercel/output/static'
 };
 
 export default nextConfig;
